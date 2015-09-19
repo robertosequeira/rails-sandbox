1e81fd5e9bf6d6e1e100447b0c53ebbb02049197
Roberto Sequeira <roberto@roberto-sequeira.com>
2015-09-19 00:56:52 -0600

    Add Mailgun support

--- Gemfile
+++ Gemfile
@@ -33,6 +33,8 @@ gem 'devise', '~> 3.5.2'
 
 gem 'bootstrap-sass', '~> 3.3.5'
 
+gem 'mailgunner', '~> 2.2.1'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,6 +60,9 @@ Rails.application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 
+  config.action_mailer.default_url_options = { :host => 'https://rails-sandbox-app.herokuapp.com/' }
+  config.action_mailer.delivery_method = :mailgun
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
